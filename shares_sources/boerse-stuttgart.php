d37178d1369cecdc51cb8e9a6692ac5b22b2104d
theunreplicated <theunreplicated@gmail.com>
2013-09-14 23:48:32 +0200

    cunsinniger commit

--- shares_sources/boerse-stuttgart.php
+++ shares_sources/boerse-stuttgart.php
@@ -13,8 +13,9 @@ class BoerseStuttgart{
               $data=$this->net->request("https://www.boerse-stuttgart.de/rd/de/aktien/factsheet?sSymbol={$symbol}");  
              preg_match("~ var windowid = '([^']{1,})'~",$data,$m);
              $windowid=$m[1];echo $windowid;
-             $url='https://www.boerse-stuttgart.de/rd/de/push/?windowid='.$windowid."&param=/webservices/quotes/index.html&lfields=STD_Bid,STD_Ask,BOERSE_STU_VolumeBid,BOERSE_STU_VolumeAsk,BOERSE_STU_BidDateFull,BOERSE_STU_BidTimeFull,BOERSE_STU_aktSpread,STD_Price,RAW_kzs,BOERSE_STU_Nebenrecht,BOERSE_STU_BND_Volume,STD_PriceDateFull,BOERSE_STU_PriceTimeFull,STD_Volume,STD_High,STD_Low,STD_Close,RAW_kzs_close,BOERSE_STU_Kursart,STD_DiffA,STD_DiffP,BOERSE_STU_Iday_HighYear,BOERSE_STU_Iday_LowYear,BOERSE_STU_Iday_52WHigh,BOERSE_STU_Iday_52WLow&lsymbols={$symbol}";  
-echo $url;            
+             //TODO:das folgende optional setzen,denn vermutlich wird gar nichts zurückgegeben,wenn die Aktie aktuell nicht gehandelt wird,also denn einfach den letzten bekannten Wert von dem vorherigen nehmen
+  //           $url='https://www.boerse-stuttgart.de/rd/de/push/?windowid='.$windowid."&param=/webservices/quotes/index.html&lfields=STD_Bid,STD_Ask,BOERSE_STU_VolumeBid,BOERSE_STU_VolumeAsk,BOERSE_STU_BidDateFull,BOERSE_STU_BidTimeFull,BOERSE_STU_aktSpread,STD_Price,RAW_kzs,BOERSE_STU_Nebenrecht,BOERSE_STU_BND_Volume,STD_PriceDateFull,BOERSE_STU_PriceTimeFull,STD_Volume,STD_High,STD_Low,STD_Close,RAW_kzs_close,BOERSE_STU_Kursart,STD_DiffA,STD_DiffP,BOERSE_STU_Iday_HighYear,BOERSE_STU_Iday_LowYear,BOERSE_STU_Iday_52WHigh,BOERSE_STU_Iday_52WLow&lsymbols={$symbol}";  
+//echo $url;            
 // echo  $this->net->request($url);
             }
     
